63742e5e169a06b8e23ffb443388260872ababb8
Muhammad Faisal <mfaisal@aucegypt.edu>
2020-11-22 20:28:57 +0200

    Create P5.py

---
+++ assignments/assignment10/P5.py
@@ -0,0 +1,4 @@
+# Please, see this one to deduce that psi leads psi'
+# https://github.com/mfaisal97/CS511_formal_methods/blob/master/assignments/assignment10/P5_1.in
+# Please, see this one to deduce that psi' does not lead to psi
+# https://github.com/mfaisal97/CS511_formal_methods/blob/master/assignments/assignment10/P5_2.in
